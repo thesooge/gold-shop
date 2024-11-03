90a3926c195a86fde92ac4010d65344b4ed646ce
thesooge <thesooge.8313@gmail.com>
2024-11-03 18:53:18 +0330

    products app admin defining

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category, ProductComment
 
 # Register your models here.
+
+
+class ProductCommentsAdmin(admin.TabularInline):
+    model = ProductComment
+    extra = 0
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductCommentsAdmin]
+
+
+admin.site.register(Category)
