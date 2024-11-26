fdb018e99eff49945fabe57eb5e6dd6213447010
Mohammadreza Baheri <thesooge.8313@gmail.com>
2024-11-26 12:33:13 +0330

    personalize admin page for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Order, OrderItem
+
 # Register your models here.
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemAdmin]
